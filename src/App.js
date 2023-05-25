41d6a95aa8413d9e84dccbd2e371b026f6656803
whyest <whyest@gmail.com>
2023-05-25 17:54:55 +0300

    modified navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+
 function App() {
   return (
     <div className='App'>
-      <h1>Hello world</h1>
+      <Navbar />
     </div>
   )
 }
